c4abf9bf2925c07fe4558e6c62199efedd6f6501
Redouan KACIMI <redouan.kacimi@outlook.com>
2022-05-16 23:26:20 +0200

    Initialized the Spring project
    with 3 Dependencies:
    +Spring Web
    +Spring Data JPA
    +MySQL Driver

---
+++ PatientsCardsManager/src/main/java/net/redouan/PatientsCardsManager/PatientsCardsManagerApplication.java
@@ -0,0 +1,13 @@
+package net.redouan.PatientsCardsManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientsCardsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientsCardsManagerApplication.class, args);
+	}
+
+}
---
+++ PatientsCardsManager/src/test/java/net/redouan/PatientsCardsManager/PatientsCardsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.redouan.PatientsCardsManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientsCardsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
