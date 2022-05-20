3dec7d4cf62f40fa57fdc1b163a5671bc2f339cc
Redouan KACIMI <redouan.kacimi@outlook.com>
2022-05-20 04:07:50 +0200

    UI interface ready to use

--- patientcardapp/src/app/app.component.ts
+++ patientcardapp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Patient } from './patient';
 import { PatientService } from './patient.service';
 
@@ -10,6 +11,8 @@ import { PatientService } from './patient.service';
 })
 export class AppComponent implements OnInit{
   public patients!: Patient[];
+  public editPatient!: Patient;
+  public deletePatient!: Patient;
 
   constructor(private patientService: PatientService) {}
 
@@ -26,5 +29,72 @@ export class AppComponent implements OnInit{
         alert(error.message);
       }
     );
+  }
+
+
+  public searchPatients(searchWord: string): void {
+    const results: Patient[] = [];
+    this.patients.forEach(patient => {
+      if(patient.name.toLowerCase().indexOf(searchWord.toLowerCase()) !== -1
+      || patient.vorname.toLowerCase().indexOf(searchWord.toLocaleLowerCase()) !== -1
+      || patient.versichertennummer.toLowerCase().indexOf(searchWord.toLowerCase()) !== -1
+      || patient.kassenname.toLowerCase().indexOf(searchWord.toLowerCase()) !== -1
+      || patient.ik === parseInt(searchWord) ){
+        results.push(patient);
+      }
+    });
+    this.patients = results;
+    if(results.length === 0 || !searchWord) {
+      this.getPatients();
+    }
+  }
+
+  public onAddPatient(addForm: NgForm): void {
+    this.patientService.addPatient(addForm.value).subscribe(
+      (response: Patient) => {
+        console.log(response);
+        this.getPatients();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+    document.getElementById('addFormDismissBtn')?.click();
+  }
+
+  public onUpdatePatient(updateForm: NgForm): void {
+    this.patientService.updatePatient(updateForm.value).subscribe(
+      (response: Patient) => {
+        console.log(response);
+        this.getPatients();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+    document.getElementById('updateFormDismissBtn')?.click();
+  }
+
+  public onDeletePatient(patientId: number):void {
+    this.patientService.deletePatient(patientId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getPatients();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+    document.getElementById('deleteFormDismissBtn')?.click();
+  }
+
+  public onOpenUpdateModal(patient: Patient): void {
+    this.editPatient = patient;
+    document.getElementById('updateModalBtn')?.click();
+  }
+
+  public onOpenDeleteModal(patient: Patient): void {
+    this.deletePatient = patient;
+    document.getElementById('deleteModalBtn')?.click();
   }
 }
