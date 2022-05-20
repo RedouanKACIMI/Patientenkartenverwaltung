9520650f3f9fa5a2f2649e28b9aedcf0fd031b5e
Redouan KACIMI <redouan.kacimi@outlook.com>
2022-05-20 05:32:32 +0200

    Added Java Documentation to the Controller Class

--- PatientsCardsManager/src/main/java/net/redouan/PatientsCardsManager/controller/PatientController.java
+++ PatientsCardsManager/src/main/java/net/redouan/PatientsCardsManager/controller/PatientController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * Controller Class to process REST requests.
@@ -23,12 +22,23 @@ public class PatientController {
         this.patientService = patientService;
     }
 
+
+    /**
+     * responding to a GET Request to get a list of All Patients
+     * @return Http responseEntity with the list and status 200
+     */
     @GetMapping("")
     public ResponseEntity<List<Patient>> getAllPatients() {
         List<Patient> patients = patientService.showAllPatients();
         return new ResponseEntity<>(patients, HttpStatus.OK);
     }
 
+
+    /**
+     * responding to a GET Request to get a specific Patient
+     * @param id of the wanted Patient
+     * @return Http responseEntity with a JSON of the Patient and status 200
+     */
     @GetMapping("/search/{id}")
     public ResponseEntity<Patient> getPatientById(@PathVariable("id") Long id) {
         Patient patient = patientService.showPatient(id);
@@ -37,6 +47,12 @@ public class PatientController {
         return new ResponseEntity<>(patient, HttpStatus.OK);
     }
 
+
+    /**
+     * Mapped to the REST Request POST to add a Patient
+     * @param patient the element to be added
+     * @return the saved element and status 201
+     */
     @PostMapping("/add")
     public ResponseEntity<Patient> postPatient(@RequestBody Patient patient) {
         Patient newPatient = patientService.addPatient(patient);
@@ -46,12 +62,24 @@ public class PatientController {
         return new ResponseEntity<>(newPatient, HttpStatus.CREATED);
     }
 
+
+    /**
+     * Mapped to the REST Request PUT to update if existing or add a Patient
+     * @param patient the element to be updated/added
+     * @return the updated/saved element and status 200
+     */
     @PutMapping("/update")
     public ResponseEntity<Patient> putPatient(@RequestBody Patient patient) {
         Patient updatePatient = patientService.updatePatient(patient);
         return new ResponseEntity<>(updatePatient, HttpStatus.OK);
     }
 
+
+    /**
+     * Mapped to the REST Request DELETE to remove  a Patient
+     * @param id of the element to be deleted
+     * @return Http sttus 200
+     */
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deletePatient(@PathVariable("id") Long id) {
         patientService.deletePatient(id);
